bd11235284b1bffc6c4fe65f480d7ff0e5803523
Lou Schillaci <lou@delloop.com>
2026-01-18 13:41:48 +0000

    Fix import path in pricing page
    Use @/lib/i18n instead of @/lib/LanguageContext

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { useLanguage } from '@/lib/LanguageContext'
+import { useLanguage } from '@/lib/i18n'
 
 export default function PricingPage() {
   const { language } = useLanguage()
